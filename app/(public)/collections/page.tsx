3d2dac52bcbef831a165073c2c4640db135f92b3
allison-pham <allisonppham@gmail.com>
2026-03-29 01:41:39 -0700

    Fix

--- app/(public)/collections/page.tsx
+++ app/(public)/collections/page.tsx
@@ -18,7 +18,7 @@ export default function Loading() {
             <RabbitHoles />
             <PassionGrowth />
             {/* <PhotoDumps /> */}
-            <ChildhoodNostalgiaWhimsy />
+            {/* <ChildhoodNostalgiaWhimsy /> */}
         </>
   )
 }
